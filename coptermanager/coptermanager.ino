f9b2f8be378b04a0f18fdc025ca7e02c84a5cb88
Andreas Gerstmayr <andihit@gmail.com>
2014-08-16 03:09:02 +0200

    accept hex chars over serial (with debug option)

--- coptermanager/coptermanager.ino
+++ coptermanager/coptermanager.ino
@@ -14,15 +14,34 @@ void setup()
     digitalWrite(13, LOW);
 }
 
+int hex2int(byte *arr, int startpos, int endpos)
+{
+    int base = 1;
+    int ret = 0;
+    for(int i = endpos; i >= startpos; i--) {
+        int val = 0;
+        if (arr[i] >= 'A' && arr[i] <= 'F')
+           val = 10 + (arr[i] - 'A');
+        else if (arr[i] >= 'a' && arr[i] <= 'f')
+           val = 10 + (arr[i] - 'a');
+        else if (arr[i] >= '0' && arr[i] <= '9')
+           val = arr[i] - '0';
+           
+        ret += val * base;
+        base *= 16;
+    }
+    return ret;
+}
+
 void loop()
 {
     if (Serial.available() >= 3) {
         #ifdef SERIAL_ASCII
             byte data[6];
             Serial.readBytes(data, 6);
-            int copterid = (data[0] - 48) * 10 + (data[1] - 48);
-            int command =  (data[2] - 48) * 10 + (data[3] - 48);
-            int value =    (data[4] - 48) * 10 + (data[5] - 48);
+            int copterid = hex2int(data, 0, 1);
+            int command = hex2int(data, 2, 3);
+            int value = hex2int(data, 4, 5);
         #else
             int copterid = Serial.read();
             int command = Serial.read();
